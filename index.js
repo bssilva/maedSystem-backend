9aa6b46cc49796f13bf1170d14a27aaccaf625d8
Bruno Santos <brunoxdsantos.bs@gmail.com>
2020-10-08 12:30:45 -0300

    Adiciona ProcFile

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const requireDir = require('require-dir');
+const dotenv = require('dotenv');
 
-//Iniciando App
-const app = express();
-app.use(express.json());
-app.use(cors());
-
+dotenv.config();
 //Conectando com Banco de Dados
-mongoose.connect("mongodb://localhost:27017/node", { useNewUrlParser: true 
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true 
                                                  ,   useUnifiedTopology: true}
                                                  );
 mongoose.set("useCreateIndex", true);
 requireDir("./src/models/Cadastro");
 requireDir("./src/models/Passagem");
 requireDir("./src/models/Usuario");
+
+
+//Iniciando App
+const app = express();
+app.use(express.json());
+app.use(cors());
 
  
 //Rotas
